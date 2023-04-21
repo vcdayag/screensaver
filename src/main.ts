1d4f5ee48f02bccce87c13999ffa7c4498518cd9
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-21 21:15:55 +0800

    feat: set example for obj loader library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css'
 import vertexShaderSourceCode from './shaders/vertex.glsl?raw';
 import fragmentShaderSourceCode from './shaders/fragment.glsl?raw';
-import { Cubevertices } from './cube.ts';
 import { mat4 } from 'gl-matrix';
+import { OBJ } from 'webgl-obj-loader';
+import { Cubevertices } from './cube.ts';
 
 function createShader(gl: WebGLRenderingContext, type: number, sourceCode: string): WebGLShader {
   // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
@@ -76,6 +77,75 @@ function drawParts(gl: WebGL2RenderingContext, vertices: Array<number>, glmode: 
   gl.drawArrays(glmode, 0, count);
 }
 
+import kyub from './cube.obj?raw';
+function useLibrary() {
+
+
+  // compile the shaders and create a shader program
+  var m = new OBJ.Mesh(kyub);
+
+  var vertexPositionAttribute = gl.getAttribLocation(program, "a_position");
+  gl.enableVertexAttribArray(vertexPositionAttribute);
+
+  const uModelMatrixPointer = gl.getUniformLocation(program, "u_model_matrix");
+  const uViewMatrixPointer = gl.getUniformLocation(program, "u_view_matrix");
+  const uProjectionMatrixPointer = gl.getUniformLocation(program, "u_projection_matrix");
+  // helper function to draw each part of the parakeet
+  const CONST_VIEWS = [0, 0, 0, 0, 0, -1, 0, 1, 0];
+  // const CONST_PROJECTION_ARRAY = [-10, 10, -10, 10, -10, 10];
+
+  var VIEWS = CONST_VIEWS;
+  // var PROJECTION_ARRAY = CONST_PROJECTION_ARRAY;
+
+  var modelMatrix = mat4.create();
+  var projectionMatrix = mat4.create();
+  mat4.ortho(projectionMatrix, -10, 10, -10, 10, -10, 10);
+  var viewMatrix = mat4.create();
+  mat4.lookAt(viewMatrix, new Float32Array(VIEWS.slice(0, 3)), new Float32Array(VIEWS.slice(3, 6)), new Float32Array(VIEWS.slice(6, 9)));
+  // var modelMatrix = glMatrix.mat4.create();
+  mat4.rotateX(modelMatrix, modelMatrix, Math.PI / 4);
+  // glMatrix.mat4.rotateY(modelMatrix, modelMatrix, Math.PI);
+  mat4.rotateZ(modelMatrix, modelMatrix, Math.PI / 4);
+  gl.uniformMatrix4fv(uModelMatrixPointer, false, new Float32Array(modelMatrix));
+  gl.uniformMatrix4fv(uViewMatrixPointer, false, new Float32Array(viewMatrix));
+  gl.uniformMatrix4fv(uProjectionMatrixPointer, false, new Float32Array(projectionMatrix));
+
+  // create and initialize the vertex, vertex normal, and texture coordinate buffers
+  // and save on to the mesh object
+  let mesh = OBJ.initMeshBuffers(gl, m);
+
+  // now to render the mesh
+  gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
+  gl.vertexAttribPointer(vertexPositionAttribute, mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+  // it's possible that the mesh doesn't contain
+  // any texture coordinates (e.g. suzanne.obj in the development branch).
+  // in this case, the texture vertexAttribArray will need to be disabled
+  // before the call to drawElements
+  // if(!mesh.textures.length){
+  //   gl.disableVertexAttribArray(program.textureCoordAttribute);
+  // }
+  // else{
+  //   // if the texture vertexAttribArray has been previously
+  //   // disabled, then it needs to be re-enabled
+  //   gl.enableVertexAttribArray(program.textureCoordAttribute);
+  //   gl.bindBuffer(gl.ARRAY_BUFFER, mesh.);
+  //   gl.vertexAttribPointer(program.textureCoordAttribute, mesh.textureBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  // }
+
+  // gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
+  // gl.vertexAttribPointer(vertexNormalAttribute, mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
+
+
+  gl.clearColor(0, 0, 0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.enable(gl.DEPTH_TEST);
+
+  gl.drawElements(gl.TRIANGLES, mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+}
+
 let canvas = document.querySelector<HTMLCanvasElement>('#screensaver')!;
 canvas.height = 360;
 canvas.width = 360;
@@ -100,10 +170,12 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 // set the program created earlier
 gl.useProgram(program);
 
-let newcube = [];
-for (let index = 0; index < Cubevertices.length; index++) {
-  for (let a = 0; a < Cubevertices[index].length; a++) {
-    newcube.push(Cubevertices[index][a] - .5);
-  }
-}
-drawParts(gl, newcube, gl.TRIANGLE_FAN, [0, 0.5, 0.5, 0.5]);
+// let newcube = [];
+// for (let index = 0; index < Cubevertices.length; index++) {
+//   for (let a = 0; a < Cubevertices[index].length; a++) {
+//     newcube.push(Cubevertices[index][a] - .5);
+//   }
+// }
+// drawParts(gl, newcube, gl.TRIANGLE_FAN, [0, 0.5, 0.5, 0.5]);
+
+useLibrary();
