eb62f8e48ca6fed1971d802f5b1c298ef6ebb3ec
Van Paul Dayag <vcdayag@up.edu.ph>
2023-04-21 18:21:24 +0800

    feat: add example of importing shaders

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import vertexShader from './shaders/vertex.glsl?raw';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -22,3 +23,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+console.log(vertexShader)
