b02cd55c2037e0afeccd9f5b531ecffd9718e221
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-03 20:59:17 +0800

    feat: add types file

---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type vec3by3 = readonly [number, number, number, number, number, number, number, number, number,];
+export type vec2by3 = readonly [number, number, number, number, number, number, ];
